506b870f8e449b820cf13f41de89e53f47a64c3e
Your Name <you@example.com>
2021-11-21 19:55:05 +0530

    v3

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "myfirst_ec2" {
    ami = var.ec2_ami
-   instance_type = var.type_ec2
+   instance_type = var.ec2_type
    key_name = var.pem_key
    
    tags = {
-   Name = var.instance.name
+   name = var.instance_name
    }
    
  }    
