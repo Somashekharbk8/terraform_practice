ce061cd5f3cea2a4b3a761dc8c3c08df15b319ca
Your Name <you@example.com>
2021-11-21 19:48:25 +0530

    v3

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output =ec2_public_ip" {
+output ="ec2_public_ip" {
 	value = aws_instance.myfirst_ec2.public_ip
 	}
 	
-output =ec2_private_ip" {
+output ="ec2_private_ip" {
 	value = aws_instance.myfirst_ec2.private_ip
 	}
