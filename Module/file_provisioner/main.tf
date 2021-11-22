291a2229509e2ae160eef754dcc9c5ddb5d42a1f
Your Name <you@example.com>
2021-11-22 13:10:07 +0530

    provisioners file

--- Module/file_provisioner/main.tf
+++ Module/file_provisioner/main.tf
@@ -4,7 +4,7 @@ resource "null_resource" "file_copy" {
 		user = var.module_ec2_user
 		agent = false
 		host = "${var.instance_public_ip}" 
-		private_key = file"{keys/New_AWS_Account_North-verginia.pem}"
+		private_key = "file{keys/New_AWS_Account_North-verginia.pem}"
 	}
 	provisioner "file" {
 		source = "files/test.txt"
